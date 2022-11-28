9b88911c3ba05dc9ef9569419b42154751cbc516
002hardik <hardikyadav72@gmail.com>
2022-11-28 19:56:25 +0530

    initial commit

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+{
+    id:"1",
+    Name:"Manoj Deshwal",
+    Age:"30",
+},{
+    id:"2",
+    Name:"Virat Kohli",
+    Age:"32",
+},{
+    id:"2",
+    Name:"Rohit Sharma",
+    Age:"33",
+}
+
+
+]
+export default Employees;
